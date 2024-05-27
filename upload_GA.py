e14c84949c3ef1de1592a579be14f8fedf51e00a
Alan <alanzhang2021@gmail.com>
2024-05-27 18:37:14 -0400

    Add Uploader Action

--- upload_GA.py
+++ upload_GA.py
@@ -1,14 +1,18 @@
+import pickle, logging, random, time, os
+
 from instagrapi import Client
+from tiktok_uploader.upload import upload_videos
+from tiktok_uploader.auth import AuthBackend 
 from instagrapi.exceptions import LoginRequired
-import logging, os, time, random, pickle
+ 
 
 USERNAME = 'chalant_ttrp'
 PASSWORD = 'alanetai2332'
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
 class Video_Object:
+
     def __init__(self, video_path, caption, user):
         self.video_path = video_path
         self.caption = caption
@@ -16,7 +20,42 @@ class Video_Object:
     
     def __repr__(self):
         return f"Video_Object(video_path={self.video_path}, caption={self.caption}, user={self.user})"
+
+
+def delete_file(file_path):
+    """
+    Delete a file at the specified path.
+
+    Args:
+    file_path (str): The path of the file to delete.
+    """
+    try:
+        os.remove(file_path)
+        print(f"File '{file_path}' has been deleted successfully.")
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+    except PermissionError:
+        print(f"Permission denied: unable to delete '{file_path}'.")
+    except Exception as e:
+        print(f"Error occurred while deleting the file '{file_path}': {e}")
+
+def pop_first_element(file_path):
+    with open(file_path, 'rb') as file:
+        my_list = pickle.load(file)
     
+    first_element = my_list.pop(0)
+    
+    with open(file_path, 'wb') as file:
+        pickle.dump(my_list, file)
+    
+    delete_file(first_element.video_path)
+
+    video_obj = {
+            'video': str(first_element.video_path),
+            'description': first_element.caption
+        }
+    return video_obj
+
 
 def login_user(cl: Client):
     """
@@ -62,42 +101,36 @@ def login_user(cl: Client):
     if not login_via_pw and not login_via_session:
         raise Exception("Couldn't login user with either password or session")
 
-def create_video_list(base_path):
-    """
-    Process all files in a directory and its subdirectories.
-
-    :param base_path: The base directory to start the recursive file listing.
-    """
-    videos = []
-    
-    with open('video_objects.pkl', 'rb') as f:
-        list_of_video_objects = pickle.load(f)
-
-    for video_object in list_of_video_objects:
-        videos.append( {
-            'video': video_object.video_path,
-            'description': video_object.caption
-        })
-
-    # logging.info(videos)
-    return videos
-
-base_directory = 'videos'  # Change this to your directory path
-videos = create_video_list(base_directory)
-
+video = pop_first_element()
 
 cl = Client()
 login_user(cl)
 
 try:
-    for video in videos:
-        video_path, description = video["video"], video["description"]
-        logging.info("Video Path: " + str(video_path))
-        logging.info("Description: " + description)
-        cl.clip_upload(video_path, description)
-        logging.info("SUCCESS!")
-        random_time = random.randint(30, 60)
-        logging.info("Sleeping for " + str(random_time) + " secs")
-        time.sleep(random_time)
+    video_path, description = video["video"], video["description"]
+    logging.info("Video Path: " + str(video_path))
+    logging.info("Description: " + description)
+    cl.clip_upload(video_path, description)
+    logging.info("Uploaded to Instagram!")
 except Exception as e:
     logging.info("Error: " + str(e))
+
+
+random_time = random.randint(8, 12)
+logging.info("Sleeping for " + str(random_time) + " secs")
+time.sleep(random_time)
+
+try:
+    auth = AuthBackend(cookies='tiktok/cookies.txt')
+except Exception as e:
+    logging.debug("Authentication error: " + str(e))
+
+temp = [video]
+try:
+    failed = upload_videos(videos=temp, auth=auth, headless=True)
+    logging.info("Uploaded to TikTok!")
+except Exception as e:
+    logging.debug("Error: " + str(e))
+        
+for video in failed: # each input video object which failed
+    logging.debug(f'{video["video"]} with description "{video["description"]}" failed')
